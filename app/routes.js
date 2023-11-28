75814d956bf52960345a1e21a7c0e8da015fc169
Tim Minor <tim.minor@gmail.com>
2023-11-28 10:38:31 +0000

    Adding routes and email conf code page

--- app/routes.js
+++ app/routes.js
@@ -7,3 +7,9 @@ const govukPrototypeKit = require('govuk-prototype-kit')
 const router = govukPrototypeKit.requests.setupRouter()
 
 // Add your routes here
+
+router.post('/new/email-address', (req, res) => {
+    res.redirect('/new/code')
+}
+
+)
